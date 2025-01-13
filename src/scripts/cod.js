81636abbe385a75e06d58d07bafd5b4e43a68249
Felipe <macedoflp@hotmail.com>
2025-01-13 20:18:17 -0300

    consideraveis alteracoes

--- src/scripts/cod.js
+++ src/scripts/cod.js
@@ -1,5 +1,14 @@
 function toggleMenu() {
-    const nav = document.querySelector('.nav');
-    nav.classList.toggle('show');
-  }
+  const nav = document.querySelector('.nav');
+  nav.classList.toggle('show');
+}
+
+document.addEventListener('click', function (event) {
+  const nav = document.querySelector('.nav');
+  const menu = document.querySelector('.menu');
   
+ 
+  if (!nav.contains(event.target) && !menu.contains(event.target)) {
+    nav.classList.remove('show');
+  }
+});
